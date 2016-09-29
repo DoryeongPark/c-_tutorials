0fcccd8d731a308e0597847c735355e49cc7a85f
Doryeong Park <zzada0520@gmail.com>
2016-09-29 20:19:53 +0900

    Comment updated

--- Assignment#2/Assignment2/Assignment2/Assignment2.cs
+++ Assignment#2/Assignment2/Assignment2/Assignment2.cs
@@ -8,46 +8,62 @@ using MetroFramework.Forms;
 
 namespace Assignment2
 {
+    /*
+     *  Members
+     *  syncVolume      :   Background thread to apply current volume 
+     *  syncBrightness  :   Background thread to apply current brightness 
+     *  muteChecker     :   Background timer to apply current mute state 
+     *  VOLUME_MAX      :   Max system volume value
+     */
     public partial class Assignment2 : MetroForm
     {
-        private Thread SyncVolume;
-        private Thread SyncBrightness;
-        private System.Windows.Forms.Timer MuteChecker;
-        private const float VOLUME_MAX = 65535;
+        private Thread syncVolume;
+        private Thread syncBrightness;
+        private System.Windows.Forms.Timer muteChecker;
+        private int VOLUME_MAX = 65535;
 
         public Assignment2()
         {
             InitializeComponent();
         }
 
+        /*
+         * Starts background thread for synchronizing with current state
+         */
         private void OnLoad(object sender, EventArgs e)
         {
-            SyncVolume = new Thread(new ThreadStart(delegate () { SyncVolumeBackground(); }));
-            SyncVolume.Start();
+            syncVolume = new Thread(new ThreadStart(delegate () { SyncVolumeBackground(); }));
+            syncVolume.Start();
 
-            SyncBrightness = new Thread(new ThreadStart(delegate () { SyncBrightnessBackground(); }));
-            SyncBrightness.Start();
+            syncBrightness = new Thread(new ThreadStart(delegate () { SyncBrightnessBackground(); }));
+            syncBrightness.Start();
 
-            MuteChecker = new System.Windows.Forms.Timer();
-            MuteChecker.Interval = 2000;
-            MuteChecker.Tick += new EventHandler(SyncMuteBackground);
-            MuteChecker.Start();
+            muteChecker = new System.Windows.Forms.Timer();
+            muteChecker.Interval = 2000;
+            muteChecker.Tick += new EventHandler(SyncMuteBackground);
+            muteChecker.Start();
 
             rb_monitoroff.Checked = true;
         }
 
+        /*
+         * Stops all threads
+         */
         private void OnDestroy(object sender, FormClosedEventArgs e)
         {
-            if (SyncVolume != null || SyncVolume.IsAlive)
-                SyncVolume.Abort();
+            if (syncVolume != null || syncVolume.IsAlive)
+                syncVolume.Abort();
 
-            if (SyncBrightness != null || SyncBrightness.IsAlive)
-                SyncBrightness.Abort();
+            if (syncBrightness != null || syncBrightness.IsAlive)
+                syncBrightness.Abort();
 
-            if (MuteChecker != null || MuteChecker.Enabled)
-                MuteChecker.Stop();
+            if (muteChecker != null || muteChecker.Enabled)
+                muteChecker.Stop();
         }
 
+        /*
+         * Update value of application's trackbar with current volume on interval
+         */
         private void SyncVolumeBackground()
         {
             CheckForIllegalCrossThreadCalls = false;
@@ -61,6 +77,9 @@ namespace Assignment2
             }
         }
 
+        /*
+         * Update value of applications trackbar with current brightness on interval
+         */
         private void SyncBrightnessBackground()
         {
             CheckForIllegalCrossThreadCalls = false;
@@ -74,48 +93,71 @@ namespace Assignment2
             }
         }
 
+        /*
+         *  Set current mute state for toggle 
+         */
         private void SyncMuteBackground(object sender, EventArgs e)
         {
             toggle_mute.Checked = AudioManager.GetMasterVolumeMute();
         }
 
-        
+        /*
+         * Change system volume with trackbar's value
+         */
         private void VolumeScrollChanged(object sender, ScrollEventArgs e)
         {
             //AudioManager.SetMasterVolume(e.NewValue);
             Nircmd("setsysvolume " + Convert.ToString((int)(VOLUME_MAX * (e.NewValue / 100.0f))));
         }
 
+        /*
+         * Resume background volume thread
+         */
         private void VolumeDragCompleted(object sender, DragEventArgs e)
         {
-            SyncVolume = new Thread(new ThreadStart(delegate () { SyncVolumeBackground(); }));
-            SyncVolume.Start();
+            syncVolume = new Thread(new ThreadStart(delegate () { SyncVolumeBackground(); }));
+            syncVolume.Start();
         }
 
+        /*
+         * Stop background volume thread
+         */ 
         private void VolumeDragStarted(object sender, DragEventArgs e)
         {
-            if (SyncVolume != null || SyncVolume.IsAlive)
-                SyncVolume.Abort();
+            if (syncVolume != null || syncVolume.IsAlive)
+                syncVolume.Abort();
         }
-
+        
+        /*
+         * Change system brightness with trackbar's value
+         */
         private void BrightnessScrollChanged(object sender, ScrollEventArgs e)
         {
             //Brightness.SetBrightness((byte)e.NewValue);
             Nircmd("setbrightness " + Convert.ToString(e.NewValue));
         }
 
+        /*
+         * Resume background brightness thread
+         */
         private void BrightnessDragCompleted(object sender, DragEventArgs e)
         {
-            SyncBrightness = new Thread(new ThreadStart(delegate () { SyncBrightnessBackground(); }));
-            SyncBrightness.Start();
+            syncBrightness = new Thread(new ThreadStart(delegate () { SyncBrightnessBackground(); }));
+            syncBrightness.Start();
         }
 
+        /*
+         * Stop background brightness thread
+         */
         private void BrightnessDragStarted(object sender, DragEventArgs e)
         {
-            if (SyncBrightness != null || SyncBrightness.IsAlive)
-                SyncBrightness.Abort();
+            if (syncBrightness != null || syncBrightness.IsAlive)
+                syncBrightness.Abort();
         }
 
+        /*
+         *  Change system mute state
+         */ 
         private void MuteToggled(object sender, EventArgs e)
         {
             //AudioManager.SetMasterVolumeMute(toggle_mute.Checked);
@@ -129,6 +171,9 @@ namespace Assignment2
             Nircmd("mutesysvolume " + option);
         }
 
+        /*
+         * Execute nircmd as radio button checked
+         */
         private void ExecuteButtonClicked(object sender, EventArgs e)
         {
             if (rb_monitoroff.Checked)
@@ -140,7 +185,10 @@ namespace Assignment2
             else if (rb_turnoff.Checked)
                 Nircmd("exitwin poweroff");
         }
-
+    
+        /*
+         * Execute nircmd program with argument
+         */
         private void Nircmd(string args)
         {
             Process p = new Process();
